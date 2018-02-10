bf019b7664280dd13ddf164cbb084071b864d3a8
David Morris <32933372+YardieBiggz@users.noreply.github.com>
2018-02-10 16:34:31 +0000

    Updates

--- dmusite/app/controllers/posts_controller.rb
+++ dmusite/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    http_basic_authenticate_with name: "david", password: "12345", except: [:index, :show]
     def index
       @posts = Post.all
     end
@@ -8,6 +9,7 @@ class PostsController < ApplicationController
     end
 
     def new
+      @post = Post.new
 
     end
 
@@ -15,9 +17,34 @@ class PostsController < ApplicationController
         #render plain: params[:post].inspect
         @post = Post.new(post_params)
 
-        @post.save
+        if(@post.save)
         redirect_to @post
+      else
+        render "new"
     end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if(@post.update(post_params))
+        redirect_to @post
+  else
+    render 'edit'
+end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
 
     private def post_params
       params.require(:post).permit(:title, :body)
