bd518b2074692248b81ac1fff6fe3ef33a7a8033
David Morris <32933372+YardieBiggz@users.noreply.github.com>
2018-02-20 07:46:24 +0000

    Implimentation
    Started to generate the devise for authentication and some controllers which i havent started using yet but just getting everything working properly.

--- DMUSocialProject/config/environments/development.rb
+++ DMUSocialProject/config/environments/development.rb
@@ -1,4 +1,9 @@
 Rails.application.configure do
+
+ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+ # For devises use
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -12,7 +17,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
--- DMUSocialProject/config/routes.rb
+++ DMUSocialProject/config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users do
+  member do
+    get :following, :followers
+  end
+end
+resources :relationships, only: [:create, :destroy]
+resources :posts
+
+  #This defines the routepage
+  root 'pages#index'
+
+  # This is the Definition Routes for Pages
+  get '/home' => 'pages#home' # override default routes.
+  get '/user/:id' => 'pages#profile'
+  get '/explore' => 'pages#explore'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
